6f762e10a8d2760755b1928d695081669d39a0f7
Miguel Cano <55782155+miguelcanosantana@users.noreply.github.com>
2026-03-25 19:23:00 +0100

    Refactor image to use Astro builtin image component.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,21 @@
 import { defineCollection } from 'astro:content';
-import { glob, file } from 'astro/loaders';
+import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 // Collections
 const items = defineCollection({
   loader: glob({ base: './src/content/items', pattern: '**/*.{md,mdx}' }),
-  schema: z.object({
+  schema: ({ image }) => z.object({
 
-    //Campos del .md
     name: z.string(),
     rating: z.number().min(1).max(5).optional(),
-    image: z.string(),
+    image: image(), 
+
     links: z.array(z.object({
       url: z.url(),
       label: z.string(),
     })).default([]).optional(),
+    
     description: z.string().optional(),
     pros: z.array(z.string()).default([]).optional(),
     cons: z.array(z.string()).default([]).optional(),
